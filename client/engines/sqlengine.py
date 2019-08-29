24836ce956e98d13db3e0288ec25e61de076f810
ZZH-WORK-PC\zzh <iceking112@gmail.com>
2019-08-29 21:16:39 +0800

    [Feature]添加因子中间库model查询，添加内码转换功能

--- client/engines/sqlengine.py
+++ client/engines/sqlengine.py
@@ -3,29 +3,34 @@
 
 """
 @version: ??
-@author: li
+@author: zzh
 @file: sqlengine.py
-@time: 2019-08-28 16:22
+@time: 2019-08-29 16:22
 """
-
+from datetime import datetime
 from typing import Iterable
 from typing import List
-from typing import Dict
-from typing import Tuple
-from typing import Union
 import pandas as pd
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from sqlalchemy import select, and_, outerjoin, join, column
 from sqlalchemy.ext.declarative import declarative_base
 
+import config
 from client.dbmodel.model import BalanceMRQ, BalanceTTM, IndicatorReport
+from client.utillities.internal_code import InternalCode
+from client.utillities.sync_util import SyncUtil
 
 
 class sqlEngine(object):
-    def __init__(self, db_url: str = None):
-        self.engine = sa.create_engine(db_url)
+    def __init__(self):
+        db = '''mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}'''.format(config.pre_db_user,
+                                                                     config.pre_db_pwd,
+                                                                     config.pre_db_host,
+                                                                     config.pre_db_port,
+                                                                     config.pre_db_database)
+        self.engine = sa.create_engine(db)
         self.session = self.create_session()
+        self._sync_util = SyncUtil()
 
     def __del__(self):
         if self.session:
@@ -41,30 +46,51 @@ class sqlEngine(object):
                       'name': name}
         return query_sets
 
+    def deal_fundamentals(self, dates, fundamentals_sets):
+        deal_df = pd.DataFrame()
+        for trade_date in dates:
+            report_date = self._sync_util.get_before_report_date(trade_date, 2)
+            trades_date_fundamentals = fundamentals_sets[
+                (fundamentals_sets['ENDDATE'] >= str(report_date)) & (
+                        fundamentals_sets['PUBLISHDATE'] <= str(trade_date))]
+            trades_date_fundamentals.sort_values(by='ENDDATE', ascending=False, inplace=True)
+            trades_date_fundamentals.drop_duplicates(subset=['COMPCODE'], keep='first', inplace=True)
+            trades_date_fundamentals['trade_date'] = datetime.strptime(str(trade_date), '%Y%m%d').date()
+            trades_date_fundamentals = trades_date_fundamentals.dropna(how='all')
+            deal_df = deal_df.append(trades_date_fundamentals)
+        deal_df.reset_index(drop=True, inplace=True)
+        return deal_df
+
     def fetch_fundamentals(self,
                            db_name: declarative_base(),
+                           db_entities: List = None,
                            db_filters: List = None,
-                           start_date=None, end_date=None,
                            dates: Iterable[str] = None):
-        query = select(db_filters).where(
-
-            db_name.PUBLISHDATE.in_(dates) if dates else db_name.PUBLISHDATE.between(start_date, end_date)
+        dates = list(set(dates))
+        dates.sort()
+        report_date = self._sync_util.get_before_report_date(dates[0], 2)
+        query = self.session.query(*db_entities, db_name.PUBLISHDATE, db_name.ENDDATE).filter(
+            db_name.PUBLISHDATE <= dates[-1], db_name.ENDDATE >= report_date, db_name.REPORTTYPE == 1
         )
-        return pd.read_sql(query, self.session.bind)
+        if db_filters is not None:
+            query = query.filter(*db_filters)
+        df = pd.DataFrame()
+        result_list = pd.read_sql(query.statement, self.session.bind)
+        if not result_list.empty:
+            df = self.deal_fundamentals(dates, result_list)
+        return df
 
 
 if __name__ == '__main__':
-    db_url = 'mysql+mysqlconnector://factor_edit:factor_edit_2019@db1.irongliang.com:3306/pre_data'
-    engine = sqlEngine(db_url)
-
+    internal = InternalCode()
+    engine = sqlEngine()
     a = engine.fetch_fundamentals(IndicatorReport, [IndicatorReport.COMPCODE,
                                                     IndicatorReport.PUBLISHDATE,
                                                     IndicatorReport.DIVCOVER,
-                                                    IndicatorReport.ROE],
-                                  dates=['20190822'])
-
-
-
-
-
-
+                                                    IndicatorReport.ROE,
+                                                    IndicatorReport.ENDDATE],
+                                  # [IndicatorReport.PUBLISHDATE <= '20190801'],
+                                  dates=['20190822', '20190818'])
+    # 内码转换
+    df = internal.join_internal_code(a, left=['trade_date', 'COMPCODE'], right=['trade_date', 'company_id'])
+    print(df)
---
+++ client/utillities/internal_code.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: ??
+@author: zzh
+@file: sqlengine.py
+@time: 2019-08-29 16:22
+"""
+
+import datetime
+import sys
+
+sys.path.append('..')
+
+import config
+import sqlalchemy as sa
+import numpy as np
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+
+
+class InternalCode(object):
+    def __init__(self):
+        source_db = '''mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}'''.format(config.rl_db_user,
+                                                                            config.rl_db_pwd,
+                                                                            config.rl_db_host,
+                                                                            config.rl_db_port,
+                                                                            config.rl_db_database)
+        # 源数据库
+        self.source = sa.create_engine(source_db)
+        # 数据库Session
+        self.session = sessionmaker(bind=self.source, autocommit=False, autoflush=True)
+        self.internal_code_table = 'gl_internal_code'
+
+    def get_Ashare_internal_code(self, trade_date=None):
+        if trade_date == None:
+            trade_date = datetime.date.today()
+        sql = """select security_code,symbol,exchange,company_id from `{0}`
+                    where exchange in (001002,001003) and security_type=101
+                     and ((begin_date<='{1}' and end_date>='{1}') 
+                     or (begin_date<='{1}' and end_date='19000101'));""".format(
+            self.internal_code_table, trade_date)
+        result_list = pd.read_sql(sql, self.source)
+        if not result_list.empty:
+            result_list['symbol'] = np.where(result_list['exchange'] == '001002',
+                                             result_list['symbol'] + '.XSHG',
+                                             result_list['symbol'] + '.XSHE')
+            result_list.drop(['exchange'], axis=1, inplace=True)
+        return result_list
+
+    def get_Ashare_internal_code_list(self, trade_dates=[]):
+        if len(trade_dates) == 0:
+            trade_dates = trade_dates.append(datetime.date.today())
+        trade_dates.sort()
+        sql = """select security_code,symbol,exchange,company_id,begin_date,end_date from `{0}`
+                    where exchange in (001002,001003) and security_type=101
+                     and ((begin_date<='{1}' and end_date>='{2}') 
+                     or (begin_date<='{1}' and end_date='19000101'));""".format(
+            self.internal_code_table, trade_dates[0], trade_dates[-1])
+        result_list = pd.read_sql(sql, self.source)
+        df = pd.DataFrame()
+        if not result_list.empty:
+            result_list['symbol'] = np.where(result_list['exchange'] == '001002',
+                                             result_list['symbol'] + '.XSHG',
+                                             result_list['symbol'] + '.XSHE')
+            for trade_date in trade_dates:
+                if not isinstance(trade_date, datetime.date):
+                    trade_date = datetime.datetime.strptime(str(trade_date), '%Y%m%d').date()
+                trade_date_code = result_list[
+                    ((result_list['begin_date'] <= trade_date) & (result_list['end_date'] >= trade_date)) | (
+                            (result_list['begin_date'] <= trade_date) & (
+                            result_list['end_date'] == datetime.datetime.strptime('19000101', '%Y%m%d').date()))]
+                trade_date_code['trade_date'] = trade_date
+                df = df.append(trade_date_code)
+        df.drop(columns=['begin_date', 'end_date', 'exchange'], axis=1, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        return df
+
+    def join_internal_code(self, df, left=[], right=[]):
+        if not df.empty:
+            if ('trade_date' in left):
+                trade_dates = list(set(df['trade_date']))
+                trade_dates.sort()
+                internal_code_sets = self.get_Ashare_internal_code_list(trade_dates)
+            else:
+                internal_code_sets = self.get_Ashare_internal_code()
+            df = pd.merge(df, internal_code_sets, left_on=left, right_on=right)
+        return df
+
+
+if __name__ == '__main__':
+    internal = InternalCode()
+    # sets = internal.get_Ashare_internal_code('20190108')
+    # print(sets)
+    sets = internal.get_Ashare_internal_code_list(['20190108', '20180101'])
+    print(sets)
