1b17037bffd00c42bee60e42bd23fc89b61fbaf3
Kerry <kerry@miglab.com>
2019-09-02 15:02:02 +0800

    消除无用信息

--- client.py
+++ client.py
@@ -5,5 +5,5 @@ from data.rebuild import Rebuild
 if  __name__=="__main__":
     #calc_engine = CalcEngine('dx')
     #print(calc_engine.remote_run('2018-12-28'))
-    rebuild = Rebuild('mysql+mysqlconnector://factor_edit:factor_edit_2019@db1.irongliang.com:3306/vision')
+    rebuild = Rebuild('mysql+mysqlconnector://123:123@127.0.0.1/vision')
     rebuild.rebuild_table('alphax.alpha191','Alpha191')
